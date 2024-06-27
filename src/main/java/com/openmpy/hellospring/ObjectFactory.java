24975d4c704b2981b9688eafefbbbea6c8d5492e
openmpy <suhwan@kakao.com>
2024-06-28 00:08:31 +0900

    구성 정보를 가져오는 다른 방법

--- src/main/java/com/openmpy/hellospring/ObjectFactory.java
+++ src/main/java/com/openmpy/hellospring/ObjectFactory.java
@@ -1,18 +1,20 @@
 package com.openmpy.hellospring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@ComponentScan
 @Configuration
 public class ObjectFactory {
 
-    @Bean
-    public PaymentService paymentService() {
-        return new PaymentService(exRateProvider());
-    }
-
-    @Bean
-    public ExRateProvider exRateProvider() {
-        return new SimpleExRateProvider();
-    }
+//    @Bean
+//    public PaymentService paymentService() {
+//        return new PaymentService(exRateProvider());
+//    }
+//
+//    @Bean
+//    public ExRateProvider exRateProvider() {
+//        return new SimpleExRateProvider();
+//    }
 }
--- src/main/java/com/openmpy/hellospring/WebApiExRateProvider.java
+++ src/main/java/com/openmpy/hellospring/WebApiExRateProvider.java
@@ -1,6 +1,7 @@
 package com.openmpy.hellospring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.IOException;
